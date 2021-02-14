15c3ae77c80180c5aec406638ca769bea36159a9
uzi0espil <ousama.esbel@gmail.com>
2021-02-14 14:09:30 +0100

    Fix code style

--- gallery/users/admin.py
+++ gallery/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 
-from gallery.users.forms import UserChangeForm, UserCreationForm, GalleryCreationForm
+from gallery.users.forms import UserChangeForm, UserCreationForm
 from gallery.users.models import Gallery
 
 User = get_user_model()
